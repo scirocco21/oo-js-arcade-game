f3f122f18678e194609b671604c77fa6a2bc38b7
Sebastian <sebgertz@gmail.com>
2018-10-02 17:19:39 -0400

    added basic end of game functionality

--- js/app.js
+++ js/app.js
@@ -32,6 +32,19 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+Enemy.prototype.checkCollisions = function() {
+  if (
+    player.x  + 40 >= this.x &&
+    this.x + 40 >= player.x &&
+    player.y + 40 >= this.y &&
+    this.y + 40 >= player.y
+    ) {
+    player.x = 200;
+    player.y = 400;
+  }
+}
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
@@ -45,7 +58,17 @@ class Player {
 }
 
 
+Player.prototype.isOver = function() {
+  return this.y <= 50 ? true : false;
+}
+
+
 Player.prototype.update = function(dt) {
+  if (this.isOver()) {
+    alert("You beat the bugs!");
+    this.x = 200;
+    this.y = 400;
+  }
 };
 
 Player.prototype.render = function() {
@@ -76,23 +99,12 @@ Player.prototype.handleInput = function(direction) {
   }
 }
 
-Enemy.prototype.checkCollisions = function() {
-    if (player.x  + 40 >= this.x &&
-        this.x + 40 >= player.x &&
-        player.y + 40 >= this.y &&
-        this.y + 40 >= player.y
-        ) {
-      player.x = 200;
-      player.y = 400;
-    }
-  }
-
 let player = new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = [new Enemy(200, 0, 230), new Enemy(100,0,146)];
+let allEnemies = [new Enemy(200, 0, 230), new Enemy(100,0,146), new Enemy(300,0,65)];
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
